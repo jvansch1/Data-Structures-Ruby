3160aa4fa9bf07dad58716d3c55c86665a6ceada
John Van Schultz <jvansch1@binghamton.edu>
2017-06-14 15:34:53 -0400

    Add comments to trickle down

--- Heap/heap.rb
+++ Heap/heap.rb
@@ -1,30 +1,40 @@
 require 'byebug'
 
+# Max Heap. Maximum element should be the root and each child should be less than the parent
+
+
 class Max_Heap
 
   attr_accessor :heap_store
 
+  # initialize heap with empty array which stores values
   def initialize
     @heap_store = []
   end
 
+  # Add value to the array and call trickle up method
+  # pass in last element of the array is the child index
   def add(value)
     @heap_store.push(value)
-    trickle_up(value, nil, @heap_store.length - 1)
+    trickle_up(value, @heap_store.length - 1)
   end
 
-  def trickle_up(value, parent_index = nil, child_index)
-    # debugger
-    parent_index ||= (child_index - 1) / 2
-    parent_value = @heap_store[parent_index]
-
+  def trickle_up(value, child_index)
+    # if child_index is it means we are at the root of the heap
     if child_index == 0
       return nil
     end
 
+    # calculate parent_index based off of child index
+    # (child_index - 1) / 2
+    parent_index = (child_index - 1) / 2
+    parent_value = @heap_store[parent_index]
+
+    # if the parent value is greater than the child value, swap the elements
+    # in the array and recursively call trickle_up with the parent_index as the new child index
     if (parent_value < value)
       @heap_store[child_index], @heap_store[parent_index] = @heap_store[parent_index], @heap_store[child_index]
-      trickle_up(value, nil, parent_index)
+      trickle_up(value, parent_index)
     end
     nil
   end
