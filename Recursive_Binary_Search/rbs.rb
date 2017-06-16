6a6dad2b8c062bd884b987f2a4892aa26ed662c9
John Van Schultz <jvansch1@binghamton.edu>
2017-06-16 18:50:42 -0400

    Add comments to Recursive_Binary_search

--- Recursive_Binary_Search/rbs.rb
+++ Recursive_Binary_Search/rbs.rb
@@ -1,19 +1,27 @@
 def binary_search(arr, target)
+  # if our arr has a length of 1 and the element is not the target,
+  # return -1 to indicate it was not found
   if arr.length <= 1 && arr[0] != target
     return -1
   end
 
   midpoint = arr.length / 2
 
+  # if target is less than midpoint, search left of midpoint
   if target < arr[midpoint]
     binary_search(arr[0...midpoint], target)
+  # if target == midpoint, return midpoint index
   elsif target == arr[midpoint]
     return midpoint
+  # if target is greater than midpoint, search right
+  # when we get a result back it will be in reference to
+  # the subset (might return 0 but mean index 4 etc.)
+  # add current midpoint + 1 in order to correctly represent index value
   else
     result = binary_search(arr[midpoint + 1..-1], target)
-    !result.nil? ? result + midpoint + 1 : nil
+    result == -1 ? result + midpoint + 1 : -1
   end
 
 end
 
-print binary_search([1,2,3,4,5,6,7,8], 1)
+print binary_search([1,2,3,4,5,6,7,8], 10)
